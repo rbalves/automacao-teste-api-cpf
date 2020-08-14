1baf09bb0334b8738f9271ea6768c0f1d05f1357
Rafael  Alves <rbalves.ads@gmail.com>
2020-08-14 14:19:42 -0400

    Refatoração

--- src/main/java/br/am/rbalves/core/factories/RestricaoFactory.java
+++ src/main/java/br/am/rbalves/core/factories/RestricaoFactory.java
@@ -1,13 +1,36 @@
 package br.am.rbalves.core.factories;
+import com.github.javafaker.Faker;
 
 public class RestricaoFactory {
 	
-	public static String buscarCpfComRestricao() {
-        return "97093236014";
+	private static String[] cpfsComRestricao = { "97093236014", "60094146012", "84809766080", "62648716050", "26276298085",
+            "01317496094", "55856777050", "19626829001", "24094592008", "58063164083" };
+
+    public static String buscarCpfComRestricao() {
+        Faker faker = new Faker();
+        int indice = faker.number().numberBetween(0, 11);
+        return cpfsComRestricao[indice];
+    }
+
+    public static Boolean verificarSeCpfExiste(String cpf) {
+        for (String item : cpfsComRestricao) {
+            if (item.equals(cpf)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String buscarCpfSemRestricao() {
-        return "99999999999";
+
+        Faker faker = new Faker();
+        String cpf = Long.toString(faker.number().randomNumber(11, true));
+
+        while (verificarSeCpfExiste(cpf)) {
+            cpf = Long.toString(faker.number().randomNumber(11, true));
+        }
+
+        return cpf;
     }
 
 }
